727b517dc2dc2a7e900f128d1fce29f3b93a2340
Mohammed Al-Juboori <mhdaljobory@gmail.com>
2018-10-22 12:01:32 +0300

    Init

---
+++ README.md
@@ -0,0 +1,7 @@
+# Pinegrow + VSCode
+
+Enables live editing with Pinegrow Web Editor. Changes done in Pinegrow are synced to VS Code and vice versa. Selecting element in Pinegrow highlights it in Visual Studio Code. Pressing CTRL+ALT+P in VS Code selects the active element in Pinegrow.
+
+Pinegrow is a powerful web editor that lets you build Bootstrap, Foundation, WordPress and plain HTML websites through UI and code.
+
+**Pinegrow 2.9 is required.**
---
+++ extension.js
@@ -0,0 +1,55 @@
+const vscode = require('vscode');
+const PinegrowVS = require('./lib/pinegrow-vs');
+
+var pgvs = new PinegrowVS();
+
+function activate(context) {
+    pgvs.init();
+
+    vscode.window.onDidChangeActiveTextEditor(function (editor) {
+        pgvs.visibleEditorChanged(editor);
+    })
+
+    vscode.workspace.onDidChangeTextDocument(function (editor) {
+        pgvs.editorTextChanged(editor);
+    });
+
+    vscode.workspace.onDidSaveTextDocument(function (document) {
+        pgvs.editorTextSaved(document);
+    });
+
+    var selectInPg = vscode.commands.registerCommand('extension.selectInPG', function () {
+        var editor = vscode.window.activeTextEditor;
+        pgvs.selectInPinegrow(editor);
+    });
+
+    var openInPg = vscode.commands.registerCommand('extension.openInPG', function () {
+        var editor = vscode.window.activeTextEditor;
+        pgvs.openInPinegrow(editor);
+    });
+
+    var reconnect = vscode.commands.registerCommand('extension.reconnect', function () {
+        pgvs.reconnect();
+    });
+
+    var detectMapping = vscode.commands.registerCommand('extension.detectMapping', function () {
+        pgvs.autoDetectMapping();
+    });
+
+    var downloadPinegrow = vscode.commands.registerCommand('extension.downloadPinegrow', function () {
+        vscode.commands.executeCommand('vscode.open', vscode.Uri.parse('http://pinegrow.com'));
+    });
+
+    var refreshPage = vscode.commands.registerCommand('extension.refreshPage', function () {
+        var editor = vscode.window.activeTextEditor;
+        pgvs.refreshPage(editor);
+    });
+
+    context.subscriptions.push(selectInPg, openInPg, reconnect, detectMapping, downloadPinegrow, refreshPage);
+}
+exports.activate = activate;
+
+function deactivate() {
+    pgvs.destroy();
+}
+exports.deactivate = deactivate;
